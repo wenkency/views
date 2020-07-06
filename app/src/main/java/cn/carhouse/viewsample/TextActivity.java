c2fd1ade18fd662922470d6db034bce57b5fae64
lven <61128910@qq.com>
2020-07-06 15:31:54 +0800

    添加XTextLayout。

---
+++ app/src/main/java/cn/carhouse/viewsample/TextActivity.java
@@ -0,0 +1,14 @@
+package cn.carhouse.viewsample;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TextActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+    }
+}
